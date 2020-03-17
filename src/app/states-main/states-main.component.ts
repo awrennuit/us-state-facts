73b665d2cce26528e6e4257ecfd42804ee6f1e50
AwrenNuit <a.rennaker@yahoo.com>
2020-03-16 23:17:18 -0500

    added list of states, condensed code so it loops through list of states

--- src/app/states-main/states-main.component.ts
+++ src/app/states-main/states-main.component.ts
@@ -7,6 +7,209 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatesMainComponent implements OnInit {
 
+  states = [
+    {
+      name: "Alabama",
+      flag: "alabama-flag.jpg"
+    },
+    {
+      name: "Alaska",
+      flag: "alaska-flag.jpg"
+    },
+    {
+      name: "Arizona",
+      flag: "arizona-flag.jpg"
+    },
+    {
+      name: "Arkansas",
+      flag: "Arkansas-flag.jpg"
+    },
+    {
+      name: "California",
+      flag: "california-flag.jpg"
+    },
+    {
+      name: "Colorado",
+      flag: "Colorado-flag.jpg"
+    },
+    {
+      name: "Connecticut",
+      flag: "connecticut-flag.jpg"
+    },
+    {
+      name: "Delaware",
+      flag: "delaware-flag.jpg"
+    },
+    {
+      name: "Florida",
+      flag: "florida-flag.jpg"
+    },
+    {
+      name: "Georgia",
+      flag: "georgia-flag.jpg"
+    },
+    {
+      name: "Hawaii",
+      flag: "hawaii-flag.jpg"
+    },
+    {
+      name: "Idaho",
+      flag: "idaho-flag.jpg"
+    },
+    {
+      name: "Illinois",
+      flag: "illinois-flag.jpg"
+    },
+    {
+      name: "Indiana",
+      flag: "indiana-flag.jpg"
+    },
+    {
+      name: "Iowa",
+      flag: "iowa-flag.jpg"
+    },
+    {
+      name: "Kansas",
+      flag: "kansas-flag.jpg"
+    },
+    {
+      name: "Kentucky",
+      flag: "kentucky-flag.jpg"
+    },
+    {
+      name: "Louisiana",
+      flag: "louisiana-flag.jpg"
+    },
+    {
+      name: "Maine",
+      flag: "maine-flag.jpg"
+    },
+    {
+      name: "Maryland",
+      flag: "maryland-flag.jpg"
+    },
+    {
+      name: "Massachusetts",
+      flag: "massachusetts-flag.jpg"
+    },
+    {
+      name: "Michigan",
+      flag: "michigan-flag.jpg"
+    },
+    {
+      name: "Minnesota",
+      flag: "minnesota-flag.jpg"
+    },
+    {
+      name: "Mississippi",
+      flag: "mississippi-flag.jpg"
+    },
+    {
+      name: "Missouri",
+      flag: "missouri-flag.jpg"
+    },
+    {
+      name: "Montana",
+      flag: "montana-flag.jpg"
+    },
+    {
+      name: "Nebraska",
+      flag: "nebraska-flag.jpg"
+    },
+    {
+      name: "Nevada",
+      flag: "nevada-flag.jpg"
+    },
+    {
+      name: "New Hampshire",
+      flag: "new-hampshire-flag.jpg"
+    },
+    {
+      name: "New Jersey",
+      flag: "new-jersey-flag.jpg"
+    },
+    {
+      name: "New Mexico",
+      flag: "new-mexico-flag.jpg"
+    },
+    {
+      name: "New York",
+      flag: "new-york-flag.jpg"
+    },
+    {
+      name: "North Carolina",
+      flag: "north-carolina-flag.jpg"
+    },
+    {
+      name: "North Dakota",
+      flag: "north-dakota-flag.jpg"
+    },
+    {
+      name: "Ohio",
+      flag: "ohio-flag.jpg"
+    },
+    {
+      name: "Oklahoma",
+      flag: "oklahoma-flag.jpg"
+    },
+    {
+      name: "Oregon",
+      flag: "oregon-flag.jpg"
+    },
+    {
+      name: "Pennsylvania",
+      flag: "pennsylvania-flag.jpg"
+    },
+    {
+      name: "Rhode Island",
+      flag: "rhode-island-flag.jpg"
+    },
+    {
+      name: "South Carolina",
+      flag: "south-carolina-flag.jpg"
+    },
+    {
+      name: "South Dakota",
+      flag: "south-dakota-flag.jpg"
+    },
+    {
+      name: "Tennessee",
+      flag: "tennessee-flag.jpg"
+    },
+    {
+      name: "Texas",
+      flag: "texas-flag.jpg"
+    },
+    {
+      name: "Utah",
+      flag: "utah-flag.jpg"
+    },
+    {
+      name: "Vermont",
+      flag: "vermont-flag.jpg"
+    },
+    {
+      name: "Virginia",
+      flag: "virginia-flag.jpg"
+    },
+    {
+      name: "Washington",
+      flag: "washington-flag.jpg"
+    },
+    {
+      name: "West Virginia",
+      flag: "west-virginia-flag.jpg"
+    },
+    {
+      name: "Wisconsin",
+      flag: "wisconsin-flag.jpg"
+    },
+    {
+      name: "Wyoming",
+      flag: "wyoming-flag.jpg"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
