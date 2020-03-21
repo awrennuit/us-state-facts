73d2d34ff1068326ce0e8104af5284b32237519c
AwrenNuit <a.rennaker@yahoo.com>
2020-03-21 12:02:41 -0500

    inital interface setup

---
+++ src/app/shared/state.ts
@@ -0,0 +1,14 @@
+export interface State {
+  $key: string;
+  abbreviation: string;
+  capital: string;
+  electoral_votes: number;
+  flag: string;
+  national_parks: number;
+  state_bird: string;
+  state_flower: string;
+  state_gemstone: string;
+  state_mammal: string;
+  state_tree: string;
+  statehood: string;
+}
